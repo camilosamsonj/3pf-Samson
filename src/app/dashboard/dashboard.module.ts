7bf39c51633e3757d12a763e0436685ddef51e9a
Camilo Samson <camilosamson@outlook.com>
2023-10-13 18:23:22 -0300

    implementando dialogs

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-
-
+import {MatToolbarModule} from '@angular/material/toolbar';import { SharedModule } from '../shared/shared.module';
+import { UsersModule } from './pages/users/users.module';
 
 @NgModule({
   declarations: [
@@ -16,8 +13,8 @@ import {MatButtonModule} from '@angular/material/button';
     CommonModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
+    SharedModule, 
+    UsersModule,
 
   ],
   exports: [
---
+++ src/app/dashboard/pages/users/users.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersComponent } from './users.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { UsersDialogComponent } from './components/users-dialog/users-dialog.component';
+
+
+
+@NgModule({
+  declarations: [
+    UsersComponent,
+    UsersDialogComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
+  exports: [
+    UsersComponent,
+  ]
+    
+})
+export class UsersModule { }
